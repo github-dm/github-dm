52c48700a451c07287bdccb90902882ec27afe6f
ne-eun <p.tyche92@gmail.com>
2022-02-13 20:54:07 +0900

    #feat: onClick 추가

--- apps/chrome-ext/src/common/components/Button.tsx
+++ apps/chrome-ext/src/common/components/Button.tsx
@@ -9,15 +9,16 @@ type Props = {
 
 const Color = {
   black:
-    "bg-black border-black text-white dark:text-black dark:bg-white dark:border-white",
+    "bg-black border-black text-white dark:text-black dark:bg-white dark:border-black",
 };
 
-export function Button({ children, color, width }: Props) {
+export function Button({ children, color, width, onClick }: Props) {
   return (
     <button
-      className={`border-1 rounded py-[0.4rem] px-[0.6rem] flex items-center ${
+      className={`border rounded py-[0.4rem] px-[0.6rem] flex items-center ${
         Color[color]
       } ${width ? `w-[${width}]` : "w-full"}`}
+      onClick={onClick}
     >
       {children}
     </button>
