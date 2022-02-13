1b80da0c22add616dca7a048a9b0868d6011210e
ne-eun <p.tyche92@gmail.com>
2022-02-13 20:55:29 +0900

    #feat: karrotframe 구성

--- apps/chrome-ext/src/App.tsx
+++ apps/chrome-ext/src/App.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
-
 import { Navigator, Screen } from "@karrotframe/navigator";
 import { fetchMessage } from "@github-dm/messaging";
-import ThemeService from "./services/theme.service";
 
 import LoginPage from "./pages/LoginPage";
 import ListPage from "./pages/ListPage";
@@ -12,49 +9,12 @@ import "./App.css";
 
 const App: React.FC = () => {
   fetchMessage();
-  const [darkEnabled, setDarkEnabled] = useState(false);
-
-  useEffect(() => {
-    setDarkEnabled(ThemeService.getTheme() !== "light");
-  }, []);
 
   return (
-    <div className="App bg-white dark:bg-black min-h-screen min-w-[380px] p-[1rem]">
-      <SwitchTest
-        checked={darkEnabled}
-        setChecked={(val) => {
-          ThemeService.setCustomTheme(val ? "dark" : "light");
-          setDarkEnabled(val);
-        }}
-      />
-      <Navigator theme="Cupertino">
-        <Screen path="/" component={LoginPage} />
-        <Screen path="/list" component={ListPage} />
-      </Navigator>
-    </div>
-  );
-};
-
-const SwitchTest: React.FC<{
-  checked: boolean;
-  setChecked: (val: boolean) => void;
-}> = ({ checked, setChecked }) => {
-  return (
-    <label className={"flex relative cursor-pointer"}>
-      <input
-        checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
-        className={"peer hidden"}
-        type={"checkbox"}
-        role={"switch"}
-      />
-      <span className={"bg-blue-300 px-6 py-3 rounded-full"} />
-      <span
-        className={
-          "absolute transition left-0 bg-blue-100 p-3 rounded-full peer-checked:translate-x-6 peer-checked:bg-blue-500"
-        }
-      />
-    </label>
+    <Navigator theme="Cupertino">
+      <Screen path="/" component={LoginPage} />
+      <Screen path="/list" component={ListPage} />
+    </Navigator>
   );
 };
 
--- apps/chrome-ext/src/pages/LoginPage.tsx
+++ apps/chrome-ext/src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useNavigator, ScreenHelmet } from "@karrotframe/navigator";
 import ThemeService from "@services/theme.service";
 import { Button } from "@UI";
@@ -5,22 +6,55 @@ import { Button } from "@UI";
 function LoginPage() {
   const theme = ThemeService.getTheme();
   const { push } = useNavigator();
+  const [darkEnabled, setDarkEnabled] = useState(false);
+
+  useEffect(() => {
+    setDarkEnabled(ThemeService.getTheme() !== "light");
+  }, []);
 
   return (
-    <>
+    <div className="h-full p-[1rem]">
       <ScreenHelmet title="" />
-      <div>
-        <h1 className="text-3xl font-bold mb-5">Github DM</h1>
-        <Button onClick={() => push("/Login")} color="black">
-          <img
-            src={`/images/github/Github-Mark-${theme}-32px.png`}
-            alt="github logo"
-          />
-          <p className="w-full text-center ">Sign in with Github</p>
-        </Button>
-      </div>
-    </>
+      <SwitchTest
+        checked={darkEnabled}
+        setChecked={(val) => {
+          ThemeService.setCustomTheme(val ? "dark" : "light");
+          setDarkEnabled(val);
+        }}
+      />
+      <h1 className="text-3xl font-bold mb-5">Github DM</h1>
+      <Button onClick={() => push("/list")} color="black">
+        <img
+          src={`/images/github/Github-Mark-${theme}-32px.png`}
+          alt="github logo"
+        />
+        <p className="w-full text-center ">Sign in with Github</p>
+      </Button>
+    </div>
   );
 }
+
+const SwitchTest: React.FC<{
+  checked: boolean;
+  setChecked: (val: boolean) => void;
+}> = ({ checked, setChecked }) => {
+  return (
+    <label className={"flex relative cursor-pointer"}>
+      <input
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        className={"peer hidden"}
+        type={"checkbox"}
+        role={"switch"}
+      />
+      <span className={"bg-blue-300 px-6 py-3 rounded-full"} />
+      <span
+        className={
+          "absolute transition left-0 bg-blue-100 p-3 rounded-full peer-checked:translate-x-6 peer-checked:bg-blue-500"
+        }
+      />
+    </label>
+  );
+};
 
 export default LoginPage;
