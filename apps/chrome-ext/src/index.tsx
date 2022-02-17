3ecd62d76c5ac600e6de2b490a3d00ccad6a4e1c
ne-eun <p.tyche92@gmail.com>
2022-02-18 02:16:56 +0900

    feat: add karrotframe/tabs

--- apps/chrome-ext/src/index.tsx
+++ apps/chrome-ext/src/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "@karrotframe/navigator/index.css";
+import "@karrotframe/tabs/index.css";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
